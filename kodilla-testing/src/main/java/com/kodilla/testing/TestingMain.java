b631e19e2e8b202b765d8130e40343157c065c1a
deseff <blazej.kurjata@gmail.com>
2019-08-28 23:00:20 +0200

    Removing "<substractingResult>" from line 6.

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -3,7 +3,7 @@ package com.kodilla.testing;
 import com.kodilla.testing.calculator.Calculator;
 import com.kodilla.testing.user.SimpleUser;
 
-public class TestingMain<substractingResult> {
+public class TestingMain {
     public static void main(String[] args) {
         System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania.");
 
