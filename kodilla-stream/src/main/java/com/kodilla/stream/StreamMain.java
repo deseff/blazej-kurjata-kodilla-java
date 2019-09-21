8405e7955134ceb68a00045fe0e39929a4a81b5e
deseff <blazej.kurjata@gmail.com>
2019-09-21 21:31:28 +0200

    Module 7.2. Tasks from theory part

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -2,6 +2,7 @@ package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
 import com.kodilla.stream.beautifier.PoemDecorator;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.*;
 import com.kodilla.stream.reference.FunctionalCalculator;
 
@@ -40,6 +41,9 @@ public class StreamMain {
         poemBeautifier.beautify("Biological hazard", text -> text.toUpperCase());
         poemBeautifier.beautify("Not important content", text -> text.toLowerCase());
         poemBeautifier.beautify("This is very important", text -> text + "!!!");
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
 
