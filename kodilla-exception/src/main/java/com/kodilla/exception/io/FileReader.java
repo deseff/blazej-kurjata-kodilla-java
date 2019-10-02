5d27d624d347293dc242ef73f4c952d803ad9386
deseff <blazej.kurjata@gmail.com>
2019-10-02 21:41:26 +0200

    Module 8.3 - Throws and throw - examples from the theory

--- kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
@@ -8,7 +8,7 @@ import java.nio.file.Paths;
 import java.util.stream.Stream;
 
 public class FileReader {
-    public void readFile() {
+    public void readFile() throws FileReaderException {
         ClassLoader classLoader = getClass().getClassLoader();
         File file = new File(classLoader.getResource("file/names.txt").getFile());
         //Path path = Paths.get(file.getPath()); //
@@ -18,13 +18,16 @@ public class FileReader {
          *try {
          *Stream<String> fileLines = Files.lines(path);
          * }
-         *----------changed into:*/
+         *----------changed into:
+        try (Stream<String> fileLines = Files.lines(Paths.get(file.getPath()))) {*/
+
 
         try (Stream<String> fileLines = Files.lines(Paths.get(file.getPath()))) {
         //try (Stream<String> fileLines = Files.lines(Paths.get("files/test.txt"))) { //intentional error
             fileLines.forEach(System.out::println);
         } catch (IOException e) {
-            System.out.println("Oh no! Something went wrong! Error:" + e);
+            throw new FileReaderException();
+            //System.out.println("Oh no! Something went wrong! Error:" + e);
         } finally {
             System.out.println("I'm gonna be here... always");
         }
