82739e058a87a66d16ca021ffd393abb7da9eb33
deseff <blazej.kurjata@gmail.com>
2020-03-13 13:18:26 +0100

    Module 4.6 - sending code to repo :)

--- src/main/java/com/kodilla/Calculator.java
+++ src/main/java/com/kodilla/Calculator.java
@@ -1,5 +1,9 @@
 package com.kodilla;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Random;
+
 public class Calculator {
 
     public int add(int a, int b) {
@@ -9,7 +13,31 @@ public class Calculator {
     public int substract(int a, int b) {
         return a - b;
     }
+
+    public String generateToken() {
+        Random random = new Random();
+        /*byte bytes[] = new byte[1];
+        random.nextBytes(bytes);
+        String token = bytes.toString();*/
+
+        int leftLimit = 48; // letter 'a'
+        int rightLimit = 122; // letter 'z'
+        int targetStringLength = 10;
+
+        String token = random.ints(leftLimit, rightLimit + 1)
+                .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97))
+                .limit(targetStringLength)
+                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                .toString();
+
+        LocalDateTime timeStamp = LocalDateTime.now();
+        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("ddMMyyyyHHmm");
+        String tokenTime = timeStamp.format(timeFormatter);
+        //String to = tokenTime.toString();
+        return token + tokenTime;
+    }
 }
+
 
 class Counting {
     public static void main(String[] args) {
@@ -19,5 +47,7 @@ class Counting {
 
         System.out.println(sum);
         System.out.println(difference);
+
+        System.out.println("Token: " + calculator.generateToken());
     }
 }
